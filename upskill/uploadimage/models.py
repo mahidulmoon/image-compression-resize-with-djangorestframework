fbe329f380e8cfd958aa3c31b4f652e9ffded21f
mahidulmoon <mahidulmoon@gmail.com>
2020-10-12 16:00:03 +0600

    upload image with reponse successfull

--- upskill/uploadimage/models.py
+++ upskill/uploadimage/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class UploadImage(models.Model):
 	image_field = models.ImageField(upload_to='images/',default='images/icon.png')
 	created_at = models.TimeField(auto_now_add=True,blank=True)
+
+
+	# def __str__(self):
+	# 	return self.image_field.url
--- upskill/uploadimage/views.py
+++ upskill/uploadimage/views.py
@@ -9,3 +9,9 @@ from .serializers import UploadImageSerializer
 class UploadImageViewSet(viewsets.ModelViewSet):
     queryset = UploadImage.objects.all()
     serializer_class = UploadImageSerializer
+
+    def create(self, request, *args, **kwargs):
+        file = request.FILES['image_field']
+        image = UploadImage.objects.create(image_field=file)
+        #image.save()
+        return Response({'message': "Uploaded","image_url" : image.image_field.url}, status=200)
